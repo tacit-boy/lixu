7ca01d2a181f348c9e247db0e25f844f0cb24648
lixu <lixu@qiniu.com>
2017-09-12 16:42:54 +0800

    抓取电影网页链接

---
+++ video_scratch.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# author: lee_xu@foxmail.com
+# date: 2017-09-11
+# 作用: 抓取剧集网页链接并输出
+# 用法：python video_scratch.py <link>
+
+import re
+import sys
+import urllib2
+
+html = urllib2.urlopen('%s' % sys.argv[1]).read()
+links = re.findall('"((ftp)s?://.*?)"', html)
+for url in links:
+    print url[0].decode('gb2312')
